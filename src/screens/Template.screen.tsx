5c2561cfdd705c2fd091192c0ba6ce0f1ca92367
Milan Camus <pro@turtletv.fr>
2022-03-08 13:27:03 +0100

    fix: 🐛 Template screen TS error

--- src/screens/Template.screen.tsx
+++ src/screens/Template.screen.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 
 interface TemplateProps {}
 
-export const TemplateScreen: React.FunctionComponent<null> = () => {
+export const TemplateScreen: React.FunctionComponent<TemplateProps> = () => {
   const { t } = useTranslation();
   React.useEffect(() => {}, []);
   return (
