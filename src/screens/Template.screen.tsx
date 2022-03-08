5499e73525ffe43f1b062d7655d561477b009af4
Milan Camus <pro@turtletv.fr>
2022-03-08 13:24:16 +0100

    feat: ✨ Add `Template` screen

---
+++ src/screens/Template.screen.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import { useTranslation } from "react-i18next";
+
+interface TemplateProps {}
+
+export const TemplateScreen: React.FunctionComponent<null> = () => {
+  React.useEffect(() => {}, []);
+  return <></>;
+};
