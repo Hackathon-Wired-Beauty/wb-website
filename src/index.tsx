738897c356881495170fcb02a1ea0588d671f4b6
Milan Camus <31384089+MisterGoodDeal@users.noreply.github.com>
2022-03-11 15:01:44 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { TestScreen } from "./screens/Test.screen";
 import { AccueilScreen } from "./screens/Accueil.screen";
-import {ScientificValidScreen} from "./screens/ScientificValid.screen";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { WhatWeDoScreen } from "./screens/WhatWeDo.screen";
 import { NomadicAndAppScreen } from "./screens/NomadicAndApp.screen";
 import { WhoAreWeScreen } from "./screens/WhoAreWe";
-import { RangeOfServiceScreen } from "./screens/RangeOfService.screen";
+import { LoginScreen } from "./screens/Login.screen";
+import { AdminScreen } from "./screens/Admin.screen";
+import { RenderScreen } from "./screens/Render.screen";
+import { DashboardScreen } from "./screens/Dashboard.screen";
 AOS.init();
 ReactDOM.render(
   <React.StrictMode>
@@ -20,11 +21,14 @@ ReactDOM.render(
         <Route path={"/"} element={<AccueilScreen />} />
         <Route path={"/what-we-do"} element={<WhatWeDoScreen />} />
         <Route path={"/who-are-we"} element={<WhoAreWeScreen />} />
-        <Route path={"/nomadic-labgrade-device-and-app"} element={<NomadicAndAppScreen />} />
-        <Route path={"/range-of-service"} element={<RangeOfServiceScreen />} />
-        <Route path={"/nomadic-labgrade-device-and-app"} element={<NomadicAndAppScreen />} />
-        <Route path={"/test"} element={<TestScreen />} />
-        <Route path={"/scientific-validation"} element={<ScientificValidScreen />} />
+        <Route
+          path={"/nomadic-labgrade-device-and-app"}
+          element={<NomadicAndAppScreen />}
+        />
+        <Route path={"/login"} element={<LoginScreen />} />
+        <Route path={"/admin"} element={<AdminScreen />} />
+        <Route path={"/dashboard"} element={<DashboardScreen />} />
+        <Route path={"/render"} element={<RenderScreen />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
